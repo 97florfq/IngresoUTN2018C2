a498ee9cadb067b8de9052ca1114d190106d1a9c
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-04 21:02:27 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,18 +4,28 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+ var num1
+ var num2
+ var resultado
+
+  num1 = document.getElementById("numeroUno").value;
+  num2 = document.getElementById("numeroDos").value;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  resultado = num1 + num2;
+  alert("La suma es " + resultado );
+
 }
 
 function restar()
 {
 	
+
 }
 
 function multiplicar()
 { 
 	
-}
 
 function dividir()
 {
