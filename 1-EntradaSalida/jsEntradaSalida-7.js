8ae374c9425abafc8d1eb2dc44458f55da453d54
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-07 20:59:10 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,32 +3,53 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
- var num1
- var num2
- var resultado
-
-  num1 = document.getElementById("numeroUno").value;
-  num2 = document.getElementById("numeroDos").value;
-  num1 = parseInt(num1);
-  num2 = parseInt(num2);
-  resultado = num1 + num2;
-  alert("La suma es " + resultado );
+{
+    var num1;
+    var num2;
+    var totals;
+    num1 = document.getElementById("numeroUno").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2 = parseInt(num2);
+    totals = num1 + num2;
+    alert("La suma es " + totals)
 
 }
-
 function restar()
 {
-	
-
+    var num1;
+    var num2;
+    var totalr;
+    num1 = document.getElementById("numeroUno").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2 = parseInt(num2);
+    totalr = num1 - num2;
+    alert("La resta es " + totalr);
 }
 
 function multiplicar()
-{ 
-	
+{
+    var num1;
+    var num2;
+    var totalm;
+    num1 = document.getElementById("numeroUno").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2 = parseInt(num2);
+    totalm = num1 * num2;
+    alert("La multiplicacion es " + totalm);
 
+}
 function dividir()
 {
-	
+    var num1;
+    var num2;
+    var totald;
+    num1 = document.getElementById("numeroUno").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2 = parseInt(num2);
+    totald = num1 / num2;
+    alert("La division es " + totald);
 }
-
