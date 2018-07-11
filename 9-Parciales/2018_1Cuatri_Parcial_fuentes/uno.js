57aac79643c7592b22c9a16a06168f0a9250e67e
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-10 22:15:50 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
-
+var largo;
+var ancho;
+var perimetro;
+largo = parseInt( prompt("Ingrese el largo"));
+ancho = parseInt( prompt("Ingrese el ancho"));
+perimetro = largo + ancho + largo + ancho;
+alert("El perimetro es " + perimetro);
 }
