20165e3730ddfb0bcf4dcb61e7ce6c063ab8501b
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-10 22:13:20 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+   if(edad >=18)
+   {
+       alert("La persona es adulto");
+   }
 
+   else if(edad >=13 && edad <=17)
+{
+    alert("La persona es adolescente");
 
+}
 
-
+else 
+{
+    alert("La persona es niño");
+}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadocivil;
+edad = document.getElementById("edad").value;
+edad = parseInt(edad)
+estadocivil = document.getElementById("estadoCivil").value;
 
-	
+if(edad <18 && estadocivil != "Soltero")
+	{
+      alert("Es muy pequeño para no ser soltero");
+    }
 
 
 }//FIN DE LA FUNCIÓN
