ba7567acbe30a9b5f7fc5ec400f747ecc9838697
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-11 21:52:25 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,20 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var max = 11;
+	var min = 1;
+	var numero = Math.floor(Math.random () * (max - min) + 1);
 
+	if(numero == 9 && 10 )
+	{
+         alert("EXCELENTE");
+	}
+	else if (numero > 4 && numero < 9)
+	{
+		alert("APROBO");
+	}
+	else
+	{
+		alert("Vamos, la proxima se puede");
+	}
 }//FIN DE LA FUNCIÓN
