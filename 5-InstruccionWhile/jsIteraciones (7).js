36c4dae898dd7e9ff557666cf5bd2fc01a201802
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-25 21:28:25 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,8 +3,16 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
 	var respuesta='si';
 
+do{
+numero = parseInt(prompt("Ingrese un numero"));
+acumulador = acumulador + numero;
+contador = contador + 1;
+respuesta= prompt("Desea ingresar otro numero?");
+respuesta = respuesta.toLowerCase();
+}while (respuesta == "si");
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,35 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
 
 
+do{
+numero = parseInt(prompt("Ingrese un numero"));
+if(numero >=0)
+{
+	positivo = positivo + numero;
+
+}
+else
+{
+negativo = negativo * numero;
+contador = contador + 1;
+
+}
+
+respuesta= prompt("Desea ingresar otro numero?");
+respuesta = respuesta.toLowerCase();
+}while (respuesta == "si");
+
 document.getElementById('suma').value=positivo;
+
+if(contador ==0)
+{
+	negativo = 0;
+}
 document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
