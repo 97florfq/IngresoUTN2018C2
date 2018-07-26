6c7b7ff3c302c109dc254626493aa48df095d360
97florfq <40808713+97florfq@users.noreply.github.com>
2018-07-25 22:10:49 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,32 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+var numero;
+var maximo;
+var minimo;
+var flag =0;
+var respuesta = "si";
+do
+{
+	numero = parseInt(prompt("Ingrese un numero"));
 
-	while(respuesta!='no')
+	
+	if( numero > maximo|| flag == 0)
 	{
-		
-	
+		maximo = numero;
 	}
+	if(numero< minimo||flag == 0)
+	{
+minimo = numero
+flag = flag++; 
+	}
+	
+	respuesta = prompt("Desea ingresar otro numero? ");
+	respuesta = respuesta.toLowerCase();
+	
 
+}while (respuesta == "si");
 
-
-
+document.getElementById("maximo").value = maximo;
+document.getElementById("minimo").value = minimo;
 }//FIN DE LA FUNCIÓN
